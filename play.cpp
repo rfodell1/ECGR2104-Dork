74cbcabdb647a07b1b171e008091194122968499
wharri41 <wharri41@uncc.edu>
2016-04-20 01:12:10 -0400

    Update play.cpp

--- play.cpp
+++ play.cpp
@@ -1,16 +1,21 @@
-//not sure if/ what needs to be included
+#include "game.cpp"
 
 void roll(){
 
   //random # generator between 1&6 to simulate a dice roll
+  int dice = rand() % 6 + 1;
   //update player position based on roll
-
+  player.setLoc() = player.getLoc() + dice;
 }
 
 void eventData(){
 
   //check current position
+  int playLoc = player.getLoc();
   //check if theres an event at that position
-  //if there is call the event handler function
-
+  for(read event file)
+    //get the location even number
+    if (playLoc = eventNum){
+      //if there is call the event handler function
+    }
 }
