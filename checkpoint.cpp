e33640e4ecf427dda71f24a412f7f87f8896f55d
rfodell1 <rfodell1@uncc.edu>
2016-04-20 09:34:53 -0400

    Update checkpoint.cpp

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -4,7 +4,7 @@ void checkpoint(){
 
   //if checkpoint is a multiple of 25
   int check = player.getLoc() % 25
-  if(check = 0){
+  if(check == 0){
     //call the save function with the players current information as the argument'
     save(player);
   }
