7b8dc3376b5aa3fc974f7d4aa6bd5159c20d25a1
wharri41 <wharri41@uncc.edu>
2016-04-19 21:26:45 -0400

    Create event.cpp

---
+++ event.cpp
@@ -0,0 +1,17 @@
+//include player information
+//include roll information
+
+void check(){
+
+  //function to check if there is an event at players current location
+
+}
+
+void handle(){
+
+  //if there is an event at players location
+  //output event/question
+  //if event change user info
+  //if question, change user info based on answer
+
+}
