94e94a0d92e38d95862a4fbf767d2b251c2d3d6e
wharri41 <wharri41@uncc.edu>
2016-04-19 22:08:17 -0400

    Update loadsave.cpp
    save function

--- loadsave.cpp
+++ loadsave.cpp
@@ -7,5 +7,25 @@ void load(String fileName){
 
 void save(){
   //write data to players file if it exists
-  //if it doesn't exist, make it then write to it
+  String fileN = player.getName() + ".dat";
+  ofstream fileName;
+  if (!fileName){
+    //file doesn't exist, create
+    ofstream fileName(fileN + ".dat")
+    //write to file
+    fileName << player.getName();
+    fileName << player.getLevel();
+    fileName << player.getEnergy();
+    fileName << player.getSteps();
+    fileName << player.getLoc();
+    
+  }
+  else{
+    //file exists, write to
+    fileName << player.getName();
+    fileName << player.getLevel();
+    fileName << player.getEnergy();
+    fileName << player.getSteps();
+    fileName << player.getLoc();
+  }
 }
