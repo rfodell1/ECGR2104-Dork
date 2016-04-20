70a631ae4bc5e8bc74b4a830f8e1fe88a949a0af
wharri41 <wharri41@uncc.edu>
2016-04-19 21:12:21 -0400

    Create checkpoint.cpp

---
+++ checkpoint.cpp
@@ -0,0 +1,8 @@
+//include player information to access # of steps
+
+void checkpoint(){
+
+  //if checkpoint is a multiple of 25
+  //call the save function with the players current information as the argument
+
+}
