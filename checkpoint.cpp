012874076221e09ed5fe688e854e6db7444c54bf
wharri41 <wharri41@uncc.edu>
2016-04-25 15:10:58 -0400

    Update checkpoint.cpp

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -3,9 +3,12 @@
 void checkpoint(){
 
   //if checkpoint is a multiple of 25
-  int check = player.getLoc() % 25
+  if (p1.getLevel == "Terrestrial" || p1.getLevel == "Cloud")
+  int check = player.getXLoc() % 25;
+  else if (p1.getLevel == "Arboretal")
+  int check = player.getYLoc() % 25;
   if(check == 0){
     //call the save function with the players current information as the argument'
-    save(player);
+    save(p1);
   }
 }
