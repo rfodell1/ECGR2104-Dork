c66297fcc9b41df63fc6d5c0eba90f7ab5d57806
rfodell1 <rfodell1@uncc.edu>
2016-04-27 00:13:41 -0400

    Update checkpoint.cpp

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -1,14 +1,21 @@
-#include "game.cpp"
+#include "dork.h"
+#include "game.h"
 
-void checkpoint(){
-
-  //if checkpoint is a multiple of 25
-  if (p1.getLevel == "Terrestrial" || p1.getLevel == "Cloud")
-  int check = player.getXLoc() % 25;
-  else if (p1.getLevel == "Arboretal")
-  int check = player.getYLoc() % 25;
-  if(check == 0){
-    //call the save function with the players current information as the argument'
-    save(p1);
-  }
+void checkpoint(Player &p1){
+	
+	int check = 0;
+  	//if checkpoint is a multiple of 25
+  	if (p1.getLevel() == "Terrestrial" || p1.getLevel() == "Aerial")
+  		check = p1.getXLoc() % 25;
+  	else if (p1.getLevel() == "Arboreal")
+  		check = p1.getYLoc() % 25;
+  	if(check < 6 && check >= 0 && p1.getSteps() > 5)
+	{
+    		//call the save function with the players current information as the argument'
+   		//save(p1);
+		printw("Game saved.");
+		char test[1];		
+		scanw("%s", test);
+			
+  	}
 }
