3f2659cae75168b0c90567ca753df10c1d39b1b1
rfodell1 <rfodell1@uncc.edu>
2016-04-27 00:15:42 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,8 +2,24 @@
 #include "dork.h"
 #include "game.h"
 
+void Player::setParam()
+{
+	//setname - to user input
+	setName(name);
+  	//setloc - default to x = 0 y = 0
+  	setXLoc(xLoc);
+  	setYLoc(yLoc);
+  	//setenergy - default to 10
+  	setEnergy(energy);
+  	//setlevel - default to terrestrial
+  	setLevel(level);
+  	//setsteps- default to 0
+	setSteps(steps);
+}	
+	
+	
 
-Player::Player(): name(" "), level("Terrestrial"), energy(0), steps(0), location(0,0)
+Player::Player(): name(" "), level("Terrestrial"), energy(10), steps(0), location(0,0), xLoc(0), yLoc(0)
 {
 
 }
@@ -33,7 +49,17 @@ vector<int> Player::getLoc()
     	return location;
 }
 
-void Player::setName(const string in_name)
+int Player::getXLoc()
+{
+	return xLoc;
+}
+
+int Player::getYLoc()
+{
+	return yLoc;
+}
+
+void Player::setName(string in_name)
 {
     	name = in_name;
 }
@@ -56,5 +82,23 @@ void Player::setSteps(int in_steps)
 void Player::setLoc(vector<int> in_loc)
 {
 	location = in_loc;
+}
+
+void Player::setXLoc(int in_xloc)
+{
+	xLoc = in_xloc;
+}
+
+void Player::setYLoc(int in_yloc)
+{
+	yLoc = in_yloc;
+}
+
+void Player::newGame()
+{
+	printw("Enter your name:\n");
+	scanw("%s", name.c_str());
+	setParam();
+		
 }
 
