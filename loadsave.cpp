bec3f5d48dbf57cad521fc1a622b2b12fd6a5932
rfodell1 <rfodell1@uncc.edu>
2016-04-27 00:18:33 -0400

    Update loadsave.cpp

--- loadsave.cpp
+++ loadsave.cpp
@@ -1,72 +1,70 @@
-#include "game.cpp"
-#include "banner.cpp"
-#include "display.cpp"
-#include "checkpoint.cpp"
+//include player information so that you can access name/ level/ etc.
+#include "dork.h"
+#include "game.h"
 
-void setParam(){
-  //setname - to user input
-  p1.setName(name);
-  //setloc - default to x = 0 y = 0
-  p1.setX(xLoc);
-  p1.setY(yLoc);
-  //setenergy - default to 10
-  p1.setEnergy(energy);
-  //setlevel - default to terrestrial
-  p1.setLevel(level);
-  //setsteps- default to 0
-  p1.setSteps(steps);
-}
-
-void load(String fileName){
-  //read in data from file name
-  String fileN = p1.getName() + ".dat";
-  ifstream fileName;
-  int count = 1;
-  //loading saved values from previous game
-  for(String line; getLine(fileName, line)){
-    if(count == 1){
-      p1.setName(getLine(fileName, line));
-      count++;
-    }
-    if(count == 2){
-      p1.setLevel(getLine(fileName, line));
-      count++;
-    }
-    if(count == 3){
-      p1.setyEnergy(getLine(fileName, line));
-      count ++;
-    }
-    if(count == 4){
-      p1.setSteps(getLine(fileName, line));
-      count ++;
-    }
-    if(count == 5){
-      p1.setLoc(getLine(fileName, line));
-    }
+/*
+void load(Player &p1){
+  
+	char l[15];
+	printw("Type the name of your saved game:\n");
+	scanw("%s", l);
+	p1.setParam();
+	string str(l);
+	string fileN = str + ".dat";
+	
+	//read in data from file name
+  	ifstream fileName(fileN);
+		if(!fileName)
+		{
+			cerr << "File does not exist." << endl;
+			exit(0);
+		}
+  	int count = 1;
+  	for(string line; getline(fileName, line);){
+    	if(count == 1){
+      		p1.setName(getline(fileName, line));
+      		count++;
+    	}
+    	if(count == 2){
+      		p1.setLevel(getline(fileName, line));
+      		count++;
+    	}
+  	if(count == 3){
+      		p1.setEnergy(getline(fileName, line));
+      		count ++;
+    	}
+    	if(count == 4){
+      		p1.setSteps(getline(fileName, line));
+      		count ++;
+    	}
+    	if(count == 5){
+      		p1.setLoc(getline(fileName, line));
+    	}
   }
 }
 
-void save(){
+void save(Player &p1){
   //write data to players file if it exists
-  String fileN = p1.getName() + ".dat";
+  string fileN = p1.getName() + ".dat";
   ofstream fileName;
   if (!fileName){
     //file doesn't exist, create
-    ofstream fileName(fileN + ".dat")
+    ofstream fileName(fileN)
     //write to file
-    fileName << p1.getName();
-    fileName << p1.getLevel();
-    fileName << p1.getEnergy();
-    fileName << p1.getSteps();
-    fileName << p1.getLoc();
+    fileName << p1.getName() << endl;
+    fileName << p1.getLevel() << endl;
+    fileName << p1.getEnergy() << endl;
+    fileName << p1.getSteps() << endl;
+    fileName << p1.getLoc() << endl;
     
   }
   else{
     //file exists, write to
-    fileName << p1.getName();
-    fileName << p1.getLevel();
-    fileName << p1.getEnergy();
-    fileName << p1.getSteps();
-    fileName << p1.getLoc();
+    fileName << p1.getName() << endl;
+    fileName << p1.getLevel() << endl;
+    fileName << p1.getEnergy() << endl;
+    fileName << p1.getSteps() << endl;
+    fileName << p1.getLoc() endl;
   }
 }
+*/
