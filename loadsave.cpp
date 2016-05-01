21ea0fafac8982d84f47e0b8b6ad54425ea7cc48
rfodell1 <rfodell1@uncc.edu>
2016-05-01 13:04:07 -0400

    Update loadsave.cpp

--- loadsave.cpp
+++ loadsave.cpp
@@ -2,13 +2,12 @@
 #include "dork.h"
 #include "game.h"
 
-/*
+
 void load(Player &p1){
   
 	char l[15];
 	printw("Type the name of your saved game:\n");
 	scanw("%s", l);
-	p1.setParam();
 	string str(l);
 	string fileN = str + ".dat";
 	
@@ -19,52 +18,70 @@ void load(Player &p1){
 			cerr << "File does not exist." << endl;
 			exit(0);
 		}
-  	int count = 1;
-  	for(string line; getline(fileName, line);){
-    	if(count == 1){
-      		p1.setName(getline(fileName, line));
-      		count++;
-    	}
-    	if(count == 2){
-      		p1.setLevel(getline(fileName, line));
-      		count++;
-    	}
-  	if(count == 3){
-      		p1.setEnergy(getline(fileName, line));
-      		count ++;
-    	}
-    	if(count == 4){
-      		p1.setSteps(getline(fileName, line));
-      		count ++;
-    	}
-    	if(count == 5){
-      		p1.setLoc(getline(fileName, line));
-    	}
-  }
+		else
+		{
+			fileName.open(fileN);
+			fileProcess(fileName, p1);
+		}				
+		
+	
 }
 
 void save(Player &p1){
-  //write data to players file if it exists
-  string fileN = p1.getName() + ".dat";
-  ofstream fileName;
-  if (!fileName){
-    //file doesn't exist, create
-    ofstream fileName(fileN)
-    //write to file
-    fileName << p1.getName() << endl;
-    fileName << p1.getLevel() << endl;
-    fileName << p1.getEnergy() << endl;
-    fileName << p1.getSteps() << endl;
-    fileName << p1.getLoc() << endl;
+  	//write data to players file if it exists
+  	string fileN = p1.getName() + "dat";  	
+	ofstream fileName(fileN);
+  	//if (!fileName){
+    		//file doesn't exist, create
+		fileName.open(fileN,ios::app);
+    		//write to file
+    		fileName << (p1.getName()).c_str() << endl;
+    		fileName << p1.getLevel() << endl;
+    		fileName << p1.getEnergy() << endl;
+		fileName << p1.getSteps() << endl;
+    		fileName << p1.getXLoc() << endl;
+		fileName << p1.getYLoc() << endl;
     
-  }
-  else{
-    //file exists, write to
-    fileName << p1.getName() << endl;
-    fileName << p1.getLevel() << endl;
-    fileName << p1.getEnergy() << endl;
-    fileName << p1.getSteps() << endl;
-    fileName << p1.getLoc() endl;
-  }
+ // 	}
+  //	else{
+    		//file exists, write to
+    //		fileName << (p1.getName()).c_str() << endl;
+    	//	fileName << p1.getLevel() << endl;
+    	//	fileName << p1.getEnergy() << endl;
+    	//	fileName << p1.getSteps() << endl;
+    	//	fileName << p1.getXLoc() << endl;
+	//	fileName << p1.getYLoc() << endl;
+  	//}
+	
+	fileName.close();
+
 }
-*/
+
+void fileProcess(ifstream &fileName, Player &p1)
+{
+	string temp[6];
+	int i = 0;
+	
+	for (string line; getline(fileName, line);){
+		temp[i] = line;
+		i++;
+    		
+  	}
+	fileName.close();
+
+}
+
+int intConvert(string &line) //stoi() gave thrown exception errors
+{
+    	string buffer = line;
+    	int a = 0;
+
+    	for(string::iterator it = buffer.begin(); it != buffer.end(); ++it)
+    	{
+    	a=(a*10) + (*it-48);
+	}
+	return a;
+}
+
+
+
