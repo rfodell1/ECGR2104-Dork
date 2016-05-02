027011d21ea6e6868848697d792b98595fcfe92e
rfodell1 <rfodell1@uncc.edu>
2016-05-02 13:35:55 -0400

    Update play.cpp

--- play.cpp
+++ play.cpp
@@ -1,27 +1,119 @@
 /*********************************************************************
-* File: play.cpp
-* Description:  Event data struct
-* 		load data from file to struct
-* 		Roll function
-* 		Turn function, which calls necessary functions for the user to take each turn
-* 		function to check users position
-* 		int convert function to replace stoi
-* Author: Tate Clark (uncc.edu)
-* Author: Wesley Harrison (wharri41@uncc.edu)
-* Edits: Robert Fodell (rfodell1@uncc.edu)
-* University: UNC Charlotte
-*
-* This program is part of UNC Charlotte course project
-* and is distributed for education purpose only.
-* All commercial rights are reserved by the author on
-* distibution of the game database, custom function , header, ascii art
-* and other related to this game pack.
-* must be properly acknowledge.
-* /
-* *******************************************************************/
+ * File: play.cpp
+ * Description:  Event data struct
+ * 		load data from file to struct
+ * 		Roll function
+ * 		Turn function, which calls necessary functions for the user to take each turn
+ * 		function to check users position
+ * 		int convert function to replace stoi
+ * Author: Tate Clark (uncc.edu)
+ * Author: Wesley Harrison (wharri41@uncc.edu)
+ * Edits: Robert Fodell (rfodell1@uncc.edu)
+ * University: UNC Charlotte
+ *
+ * This program is part of UNC Charlotte course project
+ * and is distributed for education purpose only.
+ * All commercial rights are reserved by the author on
+ * distibution of the game database, custom function , header, ascii art
+ * and other related to this game pack.
+ * must be properly acknowledge.
+ * /
+ *********************************************************************/
 
 #include "dork.h"
 #include "game.h"
+
+
+int roll(){	
+	//random # generator between 1&6 to simulate a dice roll
+	srand(time(NULL));	
+	int dice = rand() % 6 + 1;
+
+	return dice;
+}
+
+void turn(Player &p1)
+{
+	evtLoad();
+
+	posCheck(p1);	
+
+	char r[4];
+	
+	while(1)
+	{
+				
+		int d1;	
+		
+		printw("Type 'roll' to roll or type 'quit' to quit.\n");
+		scanw("%s", r);
+		if (strcmp(r,"roll")==0)
+		{	
+			d1 = roll();
+		}
+		else if(strcmp(r, "quit")==0)
+		{
+			exit(0);
+		}
+		else
+		{
+			while(strcmp(r,"roll")!=0 && strcmp(r,"quit")!=0)
+			{
+				printw("Invalid input. Type 'roll' to roll.\n");
+				scanw("%s", r);
+				d1 = roll();
+			}
+		}
+
+		//check if won, lost, or quit
+		p1.setSteps(p1.getSteps() + d1);
+		p1.setEnergy(p1.getEnergy() - 1);
+
+		//if theyre in x plane
+		if (p1.getLevel() == "Terrestrial" || p1.getLevel() == "Aerial")
+		p1.setXLoc(p1.getXLoc() + d1);
+  
+		//if theyre in y plane
+		if (p1.getLevel() == "Arboreal")
+		p1.setYLoc(p1.getYLoc() + d1);
+
+		levelCheck(p1);
+	
+		checkpoint(p1);
+
+		updateBanner(p1);
+		//loop unless won, lost, or quit
+	
+		posCheck(p1);
+
+		if (p1.getEnergy() <= 0)
+		{
+			break;
+		}
+
+	}
+	
+	if (p1.getEnergy() <= 0)
+	{
+		printw("You ran out of energy. Game over.\n");
+		char q[15];
+		printw("Type anything to quit:\n");
+		scanw("%s", q);
+		if (strcmp(q,"quit"))
+		{
+			exit(0);
+		}
+	}
+	
+}
+
+void levelCheck(Player &p1)
+{
+	
+	
+	
+}
+
 
 //Struct that holds the data from events.txt
 struct event{
@@ -70,7 +162,7 @@ void evtLoad()
 		//Conversion of event coordinates to x and y integers
 		int leng = evtlHold.length();
 		int comP = evtlHold.find(',');
-		xHold = evtlHold.substr(1, comP - 1);
+		xHold = evtlHold.substr(0, comP - 1);
 		evt[i].elX = intConvert(xHold);
 		yHold = evtlHold.substr(comP + 1, leng);
 		evt[i].elY = intConvert(yHold);
@@ -94,85 +186,24 @@ void evtLoad()
 	
 }
 
-
-
-int roll(){	
-	//random # generator between 1&6 to simulate a dice roll
-	srand(time(NULL));	
-	int dice = rand() % 6 + 1;
-
-	return dice;
-}
-
-void turn(Player &p1)
+void posCheck(Player &p1)
 {
-	char r[4];
-	
-	while(1)
-	{
-				
-		int d1;	
-		
-		printw("Type 'roll' to roll or type 'quit' to quit.\n");
-		scanw("%s", r);
-		if (strcmp(r,"roll")==0)
-		{	
-			d1 = roll();
-		}
-		else if(strcmp(r, "quit")==0)
-		{
-			exit(0);
-		}
-		else
-		{
-			while(strcmp(r,"roll")!=0 && strcmp(r,"quit")!=0)
-			{
-				printw("Invalid input. Type 'roll' to roll.\n");
-				scanw("%s", r);
-				d1 = roll();
-			}
-		}
-		
-		
+	int checkVar;
 
-		//check if won, lost, or quit
-		p1.setSteps(p1.getSteps() + d1);
-		updateBanner(p1);
-
-		//if theyre in x plane
-		if (p1.getLevel() == "Terrestrial" || p1.getLevel() == "Aerial")
-		p1.setXLoc(p1.getXLoc() + d1);
-  
-		//if theyre in y plane
-		if (p1.getLevel() == "Arboreal")
-		p1.setYLoc(p1.getYLoc() + d1);
-	
-		checkpoint(p1);
-
-		updateBanner(p1);
-		//loop unless won, lost, or quit
-
-	}
-}
-
-void posCheck(player& p1)
-{
-	
 	for(int c = 0; c < 200; c++)
 	{
-		int checkVar = check(pl.getXLoc(), p1.getYLoc(), evt[c].elX, evt[c].elY);	
+		checkVar = check(evt[c].elX, evt[c].elY, p1);	
 		if(checkVar == 1)
 		{
-			//handle(p1.getXLoc(), p1.getYLoc, p1.getEnergy, evt[c].descrip, evt[c].response, evt[c].energy, evt[c].elrX, evt[c].elrY, evt[c].questi);			
+			handle(evt[c].descrip, evt[c].response, evt[c].energy, evt[c].elrX, evt[c].elrY, evt[c].questi, p1);		
+		break;				
 		}
 		
 	}
 		
 }
 
-
-
-int intConvert(string line)
+int conversion(string line)
 {
 	string buffer = line;
 	int a = 0;
@@ -197,4 +228,3 @@ int intConvert(string line)
 	}
 	return a;
 }
-
